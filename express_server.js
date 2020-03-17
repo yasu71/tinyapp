23552eae8674aedcf38494cfc820439e641ce120
yasu71 <yasuyo71@gmail.com>
2020-03-17 02:40:11 +0000

    add a short URL

--- express_server.js
+++ express_server.js
@@ -6,8 +6,10 @@ app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  "i98kIO": "https://ca.yahoo.com/"
 }
+
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -19,6 +21,17 @@ app.get('/url.json', (req, res) => {
 
 app.get('/hello',  (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get ("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  console.log(req);
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
