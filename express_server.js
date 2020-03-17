256b5a1c7d4515cef05d7764dcb67592bf27f617
yasu71 <yasuyo71@gmail.com>
2020-03-17 20:56:02 +0000

    add how to save shortURL-longURL key-value pair to the urlDatabase when it receives a POST request to /urls

--- express_server.js
+++ express_server.js
@@ -6,8 +6,7 @@ app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
-  "i98kIO": "https://ca.yahoo.com/"
+  "9sm5xK": "http://www.google.com"
 }
 
 const bodyParser = require("body-parser");
@@ -35,7 +34,6 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  console.log(req);
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
@@ -45,11 +43,18 @@ app.listen(PORT, () => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("OK, your short URL is... " + generateRandomString()); // Respond with 'Ok' (we will replace this)
+  console.log(req.body.longURL);  // Log the POST request body to the console
+  const shortURL = generateRandomString()
+  // saving shortURL data and longURL to the /urls page
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect("/urls");
 });
 
 function generateRandomString() {
   return Math.random().toString(36).slice(7);;
 }
-generateRandomString();
+
+// app.get("/u/:shortURL", (req, res) => {
+//   const longURL = urlDatabase[req.params.shortURL];
+//   res.redirect(longURL);
+// });
