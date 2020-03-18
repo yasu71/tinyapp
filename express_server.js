05478c8b106d095f895a319836abb7b7a2612b52
yasu71 <yasuyo71@gmail.com>
2020-03-18 05:55:50 +0000

    add signin form with res.cookie

--- express_server.js
+++ express_server.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-}
+};
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,7 +20,7 @@ app.get('/url.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello',  (req, res) => {
+app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
@@ -39,19 +39,19 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`)
+  console.log(`Example app listening on port ${PORT}!`);
 });
 
 app.post("/urls", (req, res) => {
   console.log(req.body.longURL);  // Log the POST request body to the console
-  const shortURL = generateRandomString()
+  const shortURL = generateRandomString();
   // saving shortURL data and longURL to the /urls page
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect("/urls");
 });
 
 function generateRandomString() {
-  return Math.random().toString(36).slice(7);;
+  return Math.random().toString(36).slice(7);
 }
 
 app.get("/u/:shortURL", (req, res) => {
@@ -62,19 +62,23 @@ app.get("/u/:shortURL", (req, res) => {
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
-})
+});
 
 app.post("/urls/:shortURL/edit", (req, res) => {
-  console.log("test", req.params.shortURL);
+  // console.log("test", req.params.shortURL);
   const pageURL = `/urls/${req.params.shortURL}`;
-  console.log(pageURL);
   res.redirect(pageURL);
-})
+});
 
 app.post("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
   urlDatabase[shortURL] = req.body.longURL;
   let templateVars = { shortURL: shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
-})
+});
 
+app.post("/login", (req, res) => {
+  console.log("test", req.body);
+  res.cookie("username", req.body);
+  res.redirect('/urls/');
+});
